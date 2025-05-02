cc00e681a10bd9a3c1c5305b269a1c44a0f18d97
maksmolch <maksmolchdmitr@gmail.com>
2025-05-02 19:48:14 +0300

    RANDOMWALK-196: fix configuration

--- src/main/java/ru/random/walk/config/StorageAutoConfiguration.java
+++ src/main/java/ru/random/walk/config/StorageAutoConfiguration.java
@@ -1,6 +1,10 @@
 package ru.random.walk.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,13 +16,20 @@ import ru.random.walk.client.impl.StorageClientImpl;
 @EnableConfigurationProperties(StorageProperties.class)
 public class StorageAutoConfiguration {
     @Bean
-    @ConditionalOnMissingBean
-    public StorageProperties storageProperties(StorageProperties properties) {
-        return properties;
+    @ConditionalOnMissingBean(StorageClient.class)
+    public AmazonS3Client s3Client(StorageProperties properties) {
+        var credentials = new BasicAWSCredentials(properties.accessKey(), properties.secretKey());
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(properties.endpoint(), properties.region())
+                )
+                .build();
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(StorageClient.class)
     public StorageClient storageClient(AmazonS3Client s3Client, StorageProperties properties) {
         return new StorageClientImpl(s3Client, properties);
     }
